6a705d59679b9fd351c20fb1e81815841745b2b4
EscritoPrado <suporte@escritorioprado.com.br>
2020-05-21 16:22:03 -0300

    subindo versão

---
+++ js/jogo.js
@@ -0,0 +1,113 @@
+var altura
+var largura
+var vidas = 1
+var tempo =  100
+
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+	criaMosquitoTempo = 1500
+
+} else if (nivel === 'dificil') {
+	criaMosquitoTempo = 1000
+
+}else if (nivel === 'chuck') {
+	criaMosquitoTempo = 750
+}
+
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
+
+	console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+var cronometro = setInterval(function() {
+	tempo -= 1
+	if(tempo < 0) {
+		clearInterval(cronometro)
+		clearInterval(criaMosquito)
+		window.location.href = "vitoria.html"
+	}else {
+	document.getElementById('cronometro').innerHTML = tempo
+	} 
+}, 1000)
+
+// posicionando os mosquitos de forma ramdomica
+function pocisaoRandomica() {
+
+	//remove mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+
+		if (vidas > 3) {
+			window.location.href = "game-over.html"
+		} else {
+			document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+
+			vidas ++
+		}
+	}
+
+
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
+
+	// Aqui iremos criar o elemento HTML de forma programática
+	var mosquito = document.createElement('img')
+	mosquito.src = 'imagens/mosca.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+	mosquito.id = 'mosquito'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	mosquito.onclick = function() {
+		this.remove()
+	}
+
+	document.body.appendChild(mosquito)
+
+}
+
+//Deixando os mosquitos com tamanhos aleatórios
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	//console.log (classe)
+
+	switch (classe) {
+		case 0:
+			return 'mosquito1'
+
+		case 1:
+			return 'mosquito2'
+
+		case 2:
+			return 'mosquito3'
+
+	}
+}
+
+function ladoAleatorio() {
+	var lado = Math.floor(Math.random() * 2)
+	//console.log (classe)
+
+	switch (lado) {
+		case 0:
+			return 'ladoA'
+
+		case 1:
+			return 'ladoB'
+
+	}
+}
